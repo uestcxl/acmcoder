75379845e9fc1a3cf73ea5c2ff6e6db7a1c28b78
xl <zjlsxulei@gmail.com>
2015-10-06 02:35:25 +0800

    t1000

---
+++ src/t1000/Main.java
@@ -0,0 +1,15 @@
+package t1000;
+
+import java.util.Scanner;
+
+/**
+ * Created by xl on 15/10/6.
+ * 题目描述：http://acm.acmcoder.com/showproblem.php?pid=1000
+ */
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext())
+            System.out.println(scanner.nextInt() + scanner.nextInt());
+    }
+}
