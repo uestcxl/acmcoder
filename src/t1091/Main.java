2c8b1d07f2b790138b542ef7804bd93cbc7e0293
xl <zjlsxulei@gmail.com>
2015-10-13 22:59:54 +0800

    t1091 completed

--- src/t1091/Main.java
+++ src/t1091/Main.java
@@ -1,8 +1,20 @@
 package t1091;
 
+import java.util.Scanner;
+
 /**
  * Created by xl on 15/10/13.
  */
 public class Main {
-
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int m, n;
+        while (scanner.hasNext()){
+            m = scanner.nextInt();
+            n = scanner.nextInt();
+            if (m == 0 && n == 0)
+                System.exit(0);
+            System.out.println(m + n);
+        }
+    }
 }
